16ca99d0470b7409a08f4cf0d3df45f1e2319cba
Arpit Namdeo <arpitnamdeo79@gmail.com>
2024-05-03 21:52:29 +0530

    js added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.addEventListener("mousemove",movement);
 function movement(e){
    console.log("moving")
         let x = e.pageX;
-        let y = e.pageY;
+        let y = e.pageY;    
         curserEl.style.left = x + "px";
         curserEl.style.top = y + "px";
         curserEl.style.display = "block"
